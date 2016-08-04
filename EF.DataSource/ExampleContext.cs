687d3c660b5ad9c24709302de7b83dc48222bf99
Wachirawit <wachirawit.chankaew@gmail.com>
2016-08-04 20:30:31 +0700

    Create db context

---
+++ EF.DataSource/ExampleContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF.DataSource
+{
+    public class ExampleContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=GENESIS;Database=ExampleDb;Trusted_Connection=True;");
+        }
+    }
+}
